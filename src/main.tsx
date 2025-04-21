2a25aea204a11ff3402fa1cead3ecf2ab23b7a65
remy.baranx@gmail.com <remy.baranx@gmail.com>
2025-04-21 15:53:16 +0100

    chore: frontend setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "./Provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Provider />
+  </React.StrictMode>
+);
---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Layout } from "./pages/Layout";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            }
+        ]
+    }
+]);
